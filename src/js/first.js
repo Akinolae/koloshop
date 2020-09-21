43652453d642d9ace551a2077d7ec5629e41dfbd
Akinolae <makindeakinola22@gmail.com>
2020-09-21 12:30:37 +0100

    made a new commit

--- src/js/first.js
+++ src/js/first.js
@@ -4,31 +4,19 @@ const card_img = document.getElementById('card_img');
 const timbs = document.getElementById('timbs');
 const reg_err = document.getElementById('reg_err');
 let counter = 1;
-
-
-setInterval(() => {
-    timbs.style.transform = 'scale(1.2)';
-}, 1000);
-
-setInterval(()=>{
-    reg_err.style.display = "none";
-}, 4000)
-
-setInterval(() => {
-    timbs.style.transform = 'scale(1)';
-}, 2000);
 const shoe_name = document.getElementById('shoe_name');
 const phone_price = document.getElementById('phone_price');
 
 const shoe_price = document.getElementById('shoe_price');
-const total = parseInt(shoe_price.innerHTML) + parseInt(phone_price.innerHTML);
+// const total = parseInt(shoe_price.innerHTML) + parseInt(phone_price.innerHTML);
 
 function getData () {
     fetch(`http://localhost:8000/users/${user_id}`).then((res)=> res.json()).then((data) => console.log(data))
 }
 // getData();
 
-let arr = ['first', 'second', 'third', 'fourth', 'fifth'];
+let arr = [5, 1, 0,8,34, 22,3];
+let newArr = []
 let obj = {
     first: "Ak",
     second: 'ak2',
@@ -36,11 +24,7 @@ let obj = {
     four: 'ak4'
 }
 
-function objData() {
-}
 function arrData() {
-    arr.map((array, i) => {
-        console.log(array);
-    })
+    console.log(arr.sort())
 }
 arrData();
